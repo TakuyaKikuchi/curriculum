aa7c899e0d90e4a3a5e19041873835fca89d1f80
Takuya Kikuchi <takuya-kikuchi@cc-lesmains.co.jp>
2017-03-13 15:14:00 +0900

    no message

--- docs/RAILWAY_INFO/js/railway.js
+++ docs/RAILWAY_INFO/js/railway.js
@@ -15,13 +15,23 @@ $(function(){
 			url: root + selectRail,
 			dataType: "jsonp"
 		}).then(function(data) {
+			console.log(data);
 			var stationNum = data.response.station;
 			for (var i = 0; i < stationNum.length; i++){
 				line = stationNum[i].line;
 				name = stationNum[i].name;
 				next = stationNum[i].next;
 				prev = stationNum[i].prev;
-				list += "<li>" + name + "</li>";
+				length = i;
+				x = stationNum[i].x;
+				y = stationNum[i].y;
+				list +=
+				'<li>' +
+					'<div class=' + '"' + 'pulldown_desc' + ' passive' + '"' + '>' + name + '</div>' +
+					'<div class=' + '"' + 'num_' + length + '"' + ' style=' + 'display:none' + '>' +
+						'<iframe width=' + '"' + '100%' + '"' + 'height=' + '"' + '250' + '"' + 'frameborder=' + '"' + '0' + '"' + 'scrolling=' + '"' + 'no' + '"' + 'marginheight=' + '"' + '0' + '"' + 'marginwidth=' + '"' + '0' + '"' + 'src=' + '"' + 'http://maps.google.co.jp/maps?ll=' + x + ',' + y + '&q=' + name + '&output=embed&t=m&z=18' + '"' + '></iframe>' +
+					'</div>' +
+				'</li>';
 			}
 			$('.section_inr').append("<ul class=" + 'railway_list' + ">" + list + "</ul>");
 		});
